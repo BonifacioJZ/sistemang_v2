2ffbf8a7f17970d2105c3a474ebec4e03d5876fe
Bonifacio Juarez <revanjz6@gmail.com>
2020-03-28 16:38:44 -0600

    first commit

---
+++ database/migrations/2020_03_28_214515_create_citas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCitasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('citas', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100)->nullable();
+            $table->text('descripcion');
+            $table->date('fecha');
+            $table->time('hora_de_inicio');
+            $table->time('hora_de_finalizacion');
+            $table->boolean('status');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('citas');
+    }
+}
